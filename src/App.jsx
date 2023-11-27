c42a87b536f52acb9f11f8f6969a28a5cf1fb90d
Valentave <valentintaverna99@gmail.com>
2023-11-27 14:02:22 -0300

    Add a simple home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet, useLocation, Link } from 'react-router-dom';
 import Navbar from "./Hooks/Navbar"
 
 
 function App() {
   useLocation();
 
+  const isHomePage = location.pathname === "/";
+
   return (
     <>
       <Navbar/>
+      {isHomePage && (
+        <div>
+          <h1>Welcome to E-commerce!</h1>
+          <p>Take a look at our full product catalog or browse through the different sections of our marketplace.</p>
+          <Link to="shop">Shop</Link>
+        </div>
+      )}
       <Outlet />
     </>
   )
