3967c88ebaf5ed5f5a4e26fbbad9b1122b64516f
Valentave <valentintaverna99@gmail.com>
2023-11-27 01:02:09 -0300

    Add filter and sort options

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -1,47 +1,112 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from 'react-router-dom';
 import ProductCard from "../../Hooks/ProductCard";
-import LoadingGif from "../../assets/loading.gif"
+import LoadingGif from "../../assets/loading.gif";
 
 function Shop() {
+    const [originalProducts, setOriginalProducts] = useState([]);
     const [products, setProducts] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [minPrice, setMinPrice] = useState('');
+    const [maxPrice, setMaxPrice] = useState('');
+    const [searchTerm, setSearchTerm] = useState('');
+    const [sortOption, setSortOption] = useState(''); // Nuevo estado para la opción de orden
     const params = useParams();
-    console.log(params)
-    useNavigate();
+    const navigate = useNavigate();
 
     useEffect(() => {
-        if (Object.keys(params).length === 0){
-            fetch('https://fakestoreapi.com/products')
-                .then((response) => response.json())
-                .then((response) => {
-                    setProducts([...response]);
-                    setIsLoading(false);
-                });
-        } else {
-            fetch('https://fakestoreapi.com/products/category/' + params.category)
+        setIsLoading(true);
+        let apiUrl = 'https://fakestoreapi.com/products';
+
+        if (Object.keys(params).length > 0) {
+            apiUrl = `https://fakestoreapi.com/products/category/${params.category}`;
+        }
+
+        fetch(apiUrl)
             .then((response) => response.json())
             .then((response) => {
+                setOriginalProducts([...response]);
                 setProducts([...response]);
                 setIsLoading(false);
-            })
+            });
+    }, [params]);
+
+    useEffect(() => {
+        let filteredProducts = [...originalProducts];
+
+        if (minPrice !== '') {
+            filteredProducts = filteredProducts.filter(product => product.price >= parseFloat(minPrice));
         }
-      }, [params]);
+        if (maxPrice !== '') {
+            filteredProducts = filteredProducts.filter(product => product.price <= parseFloat(maxPrice));
+        }
+        if (searchTerm !== '') {
+            filteredProducts = filteredProducts.filter(product => product.title.toLowerCase().includes(searchTerm.toLowerCase()));
+        }
+
+        switch (sortOption) {
+            case 'price-min-to-max':
+                filteredProducts.sort((a, b) => a.price - b.price);
+                break;
+            case 'price-max-to-min':
+                filteredProducts.sort((a, b) => b.price - a.price);
+                break;
+            case 'alphabetic':
+                filteredProducts.sort((a, b) => a.title.localeCompare(b.title));
+                break;
+            case 'alphabetic-inverted':
+                filteredProducts.sort((a, b) => b.title.localeCompare(a.title));
+                break;
+            default:
+                break;
+        }
+
+        setProducts(filteredProducts);
+    }, [minPrice, maxPrice, searchTerm, originalProducts, sortOption]);
 
     return (
         <>
-        {isLoading && 
-        <div>
-            <img src={LoadingGif} />
-            <p>Loading...</p>  
-        </div>}
-        {!isLoading &&
-        <div className="products-grid">
-            {products.map((product) => (
-                <ProductCard key={product.id} product={product}/>))}
-        </div>}
+            <div>
+                <div>
+                    <label htmlFor="minPrice">Min Price:</label>
+                    <input type="number" id="minPrice" value={minPrice} onChange={(e) => setMinPrice(e.target.value)} />
+                </div>
+                <div>
+                    <label htmlFor="maxPrice">Max Price:</label>
+                    <input type="number" id="maxPrice" value={maxPrice} onChange={(e) => setMaxPrice(e.target.value)} />
+                </div>
+                <div>
+                    <label htmlFor="searchTerm">Search:</label>
+                    <input type="text" id="searchTerm" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+                </div>
+
+                <div>
+                    <label htmlFor="sortOption">Sort by:</label>
+                    <select id="sortOption" value={sortOption} onChange={(e) => setSortOption(e.target.value)}>
+                        <option value="">Select an option</option>
+                        <option value="price-min-to-max">Price (Min to Max)</option>
+                        <option value="price-max-to-min">Price (Max to Min)</option>
+                        <option value="alphabetic">Alphabetic (A-Z)</option>
+                        <option value="alphabetic-inverted">Alphabetic (Z-A)</option>
+                    </select>
+                </div>
+            </div>
+
+            {isLoading &&
+                <div>
+                    <img src={LoadingGif} alt="Loading" />
+                    <p>Loading...</p>
+                </div>
+            }
+            {!isLoading &&
+                <div className="products-grid">
+                    {products.map((product) => (
+                        <ProductCard key={product.id} product={product} />
+                    ))}
+                </div>
+            }
         </>
     )
 }
 
-export default Shop
+export default Shop;
